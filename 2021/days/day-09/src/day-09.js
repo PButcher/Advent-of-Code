8758328db0665c4f224b7049cc77981c2376d10b
pbutcher <pbutcher93@gmail.com>
2021-12-10 22:53:31 +0000

    fix back links. Add day 9 boilerplate

---
+++ 2021/days/day-09/src/day-09.js
@@ -0,0 +1,25 @@
+(async function day08() {
+  const input = await fetch("input/input.txt");
+  const res = await input.text();
+  const parsedInput = res.trim().split("\n");
+
+  // Puzzle 1
+  function puzzle1() {
+    return 0;
+  }
+
+  // Puzzle 2
+  function puzzle2() {
+    return 0;
+  }
+
+  // Run puzzles
+  var puzzle1Answer = puzzle1();
+  var puzzle2Answer = puzzle2();
+
+  // Output puzzles
+  document.getElementById("answer1").innerText = puzzle1Answer;
+  document.getElementById("answer2").innerText = puzzle2Answer;
+  console.log(`Day 01 Puzzle 1 Answer: ${puzzle1Answer}`);
+  console.log(`Day 01 Puzzle 2 Answer: ${puzzle2Answer}`);
+})();
